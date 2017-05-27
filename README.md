56a710709db4d6121f1b4fc7dffc0699ae737648
Erin Wilder <erinwilder1@gmail.com>
2017-05-27 14:14:44 -0500

    spun up local web server, templated universal elements, created work directory

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 ## UI / UX Designer // Front-End Developer
 
 A portfolio of design work by Erin Wilder.
+
+## How to run website
+
+### Spin up local web server
+
+- Terminal: Navigate to local site directory
+- Terminal: python -m SimpleHTTPServer
+- Browser: Navigate to http://localhost:8000
+
+### Include Typekit
+
+- Create Typekit in Adobe
+- Kit Settings: *.[site url],localhost
+- Kit cannot be included in template file and loaded via .load(). Embed code must be directly on each HTML page
--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,51 @@
 $(document).ready(function() {
 
-    var menu = document.getElementById('menu');
-    menu.addEventListener("click", function(){
-        if (menu.classList.contains('close')) {
-            $('#nav ul li:last-child').velocity({bottom:-400});
-            setTimeout(function() {
-                $('#nav ul li:nth-child(2)').velocity({bottom:-400});
-            }, 50);
-            setTimeout(function() {
-                $('#nav ul li:first-child').velocity({bottom:-400});
-            }, 100);
+    /* Load template pieces on pages */
+    $('header').load('template/header.html', function(){ // loads header.html in the header tag
 
-            $('#nav').velocity({opacity:0});
-            $('#nav ul').velocity({opacity:0});
-            $('#home>div').removeClass('zoomOut');
-            $('#menu').removeClass('close');
-            $('#menu .line').removeClass('rotate');
+        /* Menu / Navigaton functionality */
+        var menu = document.getElementById('menu'); //get menu button
+        menu.addEventListener("click", function(){ //list for click
+            if (menu.classList.contains('close')) { //if nav is open
+                $('#nav ul li:last-child').velocity({bottom:-400});
+                setTimeout(function() {
+                    $('#nav ul li:nth-child(2)').velocity({bottom:-400});
+                }, 50);
+                setTimeout(function() {
+                    $('#nav ul li:first-child').velocity({bottom:-400});
+                }, 100);
 
-            setTimeout(function() {
-                document.getElementById('nav').style.display='none';
-            }, 300);
-        }
-        else {
-            document.getElementById('nav').style.display='flex';
+                $('#nav').velocity({opacity:0});
+                $('#nav ul').velocity({opacity:0});
+                $('#home>div').removeClass('zoomOut');
+                $('#menu').removeClass('close');
+                $('#menu .line').removeClass('rotate');
 
-            $('#nav').velocity({opacity:1});
-            $('#nav ul').velocity({opacity:1});
-            $('#home>div').addClass('zoomOut');
-            $('#menu').addClass('close');
-            $('#menu .line').addClass('rotate');
-            
-            setTimeout(function() {
-                $('#nav ul li:first-child').velocity({bottom:0});
-            }, 50);
-            setTimeout(function() {
-                $('#nav ul li:nth-child(2)').velocity({bottom:0});
-            }, 100);
-            setTimeout(function() {
-                $('#nav ul li:last-child').velocity({bottom:0});
-            }, 150);
-        }
+                setTimeout(function() {
+                    document.getElementById('nav').style.display='none';
+                }, 300);
+            }
+            else { //if nav is closed
+                document.getElementById('nav').style.display='flex';
+
+                $('#nav').velocity({opacity:1});
+                $('#nav ul').velocity({opacity:1});
+                $('#home>div').addClass('zoomOut');
+                $('#menu').addClass('close');
+                $('#menu .line').addClass('rotate');
+                
+                setTimeout(function() {
+                    $('#nav ul li:first-child').velocity({bottom:0});
+                }, 50);
+                setTimeout(function() {
+                    $('#nav ul li:nth-child(2)').velocity({bottom:0});
+                }, 100);
+                setTimeout(function() {
+                    $('#nav ul li:last-child').velocity({bottom:0});
+                }, 150);
+            }
+        });
+
     });
 
 });
